86fb58db3a535a0b27687712af0e21a1983be260
tanopanta <25007602+tanopanta@users.noreply.github.com>
2022-12-16 14:32:21 +0900

    add tape test

--- src/main/java/sample/OkreplaySample.java
+++ src/main/java/sample/OkreplaySample.java
@@ -7,13 +7,22 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class OkreplaySample {
+    private OkHttpClient client;
+
+    public OkreplaySample() {
+        this.client = new OkHttpClient.Builder().build();
+    }
+
+    public OkreplaySample(OkHttpClient client) {
+        this.client = client;
+    }
+
     public Response Get() {
-        OkHttpClient client = new OkHttpClient.Builder().build();
         Request request = new Request.Builder().url("https://httpbin.org/get").build();
 
         Response response = null;
         try {
-            response = client.newCall(request).execute();
+            response = this.client.newCall(request).execute();
         } catch (IOException e) {
             // TODO: handle exception
         }
--- src/test/java/sample/OkreplaySampleTest.java
+++ src/test/java/sample/OkreplaySampleTest.java
@@ -1,21 +1,35 @@
 package sample;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
-
+import okhttp3.OkHttpClient;
 import okhttp3.Response;
+import okreplay.OkReplayConfig;
+import okreplay.OkReplayInterceptor;
+import okreplay.Recorder;
+import okreplay.TapeMode;
 
 public class OkreplaySampleTest {
     @Test
     public void testGet() {
-        OkreplaySample sut = new OkreplaySample();
+        OkReplayConfig configuration = new OkReplayConfig.Builder().defaultMode(TapeMode.READ_WRITE)
+                .interceptor(new OkReplayInterceptor()).build();
+
+        OkHttpClient client =
+                new OkHttpClient.Builder().addInterceptor(configuration.interceptor()).build();
+        OkreplaySample sut = new OkreplaySample(client);
+
+        Recorder rec = new Recorder(configuration);
+        rec.start("test");
 
         Response res = sut.Get();
-
         assertNotNull(res);
         assertTrue(res.isSuccessful());
         assertEquals(res.code(), 200);
+
+        rec.stop();
     }
 }
